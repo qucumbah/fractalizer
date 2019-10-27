12c3997e19910c0f75cd12d39a6760bed496e9a1
qucumbah <sc.buher@yandex.ru>
2019-10-27 16:16:19 +0300

    fix serviceworker path

--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     // uncomment for production
-    navigator.serviceWorker.register('/serviceWorker.js')
+    navigator.serviceWorker.register('./serviceWorker.js')
       .then(registration => console.log(registration))
       .catch(error => console.log(error))
   });
