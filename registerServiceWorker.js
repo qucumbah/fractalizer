4d1ce67d1920f2dc240cd901ce2dcf0a3c57624f
qucumbah <sc.buher@yandex.ru>
2019-10-27 15:41:23 +0300

    fix styles

--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,8 +1,8 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     // uncomment for production
-    // navigator.serviceWorker.register('/serviceWorker.js')
-      // .then(registration => console.log(registration))
-      // .catch(error => console.log(error))
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then(registration => console.log(registration))
+      .catch(error => console.log(error))
   });
 }
